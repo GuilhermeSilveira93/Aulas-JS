06eaea706d01dbba675d27e9d60295d854f516ca
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-06-22 20:42:39 -0300

    1.1
    Varias aulas sendo feitas.

---
+++ CURSO EM VIDEO/modulo D - Condições em JavaScript/aula 11 - Codições Pt1/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if ( vel > 60){ // CONDIÇÃO SIMPLES, QUER DIZER QUE NÃO TEM O ELSE - SE NÃO.
+    console.log('Você ultrapassou a velocidade permitida. MULTADO !')
+}
+console.log('Dirija sempre com sinto de segurança !')
---
+++ CURSO EM VIDEO/modulo D - Condições em JavaScript/aula 11 - Codições Pt1/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais=='Brasil'){
+    console.log('Ai sim quebrada !, tá ligado que tamo junto !')
+}else{
+    console.log('Amigão, você é gringo ? Aqui é Brasil mano !')
+}
