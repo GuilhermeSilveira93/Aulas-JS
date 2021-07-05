8b68d8d7a3e7e05fb3418639da9cd91c8ac084b4
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-07-05 20:40:21 -0300

    10

--- CURSO EM VIDEO/modulo E - Repetições/aula 14 - Repetições Pt2/for.js
+++ CURSO EM VIDEO/modulo E - Repetições/aula 14 - Repetições Pt2/for.js
@@ -1,4 +1,11 @@
-var c = 2
-for( c ; c<=10; c+2){
-    console.log(`Número ${c}`)
+var c = 1
+console.log('Vai Começar...')
+for( c ; c<=10; c++){
+    console.log(`Número ${c} é :`)
+    if(c % 2 == 0){
+        console.log("par")
+    } else {
+        console.log("impar")
+    }
 }
+console.log('Fim !')
