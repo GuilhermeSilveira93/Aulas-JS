ade5f9e5f89c346b96ce9abdf0a8636770c7d7ed
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-07-13 22:13:22 -0300

    1.0

--- CURSO EM VIDEO/modulo E - Repetições/Exercicios/ex003 copy/js/script.js
+++ CURSO EM VIDEO/modulo E - Repetições/Exercicios/ex003 copy/js/script.js
@@ -6,10 +6,11 @@ function clicar(){
         }else{
             let n = Number(num.value)
             let c = 1
+            tab.innerHTML = ''
                 for(let c = 1; c <=10 ; c++){
                     let item = document.createElement('option')
                     item.text = `${n} x ${c} = ${n*c}`
                     tab.appendChild(item)
                 }
         }
-}
+} 
---
+++ CURSO EM VIDEO/modulo F - Avançando os Estudos/aula 15 - Variáveis Compostas/ambiente.js
@@ -0,0 +1,4 @@
+//VARIAVEL COMPOSTA//
+var num = [10,8,'Guilherme',15]
+
+console.log(`Nosso valor é o ${num.sort()}`)
